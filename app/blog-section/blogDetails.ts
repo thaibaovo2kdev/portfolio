c69dab97fa4e003fee838acb14aae8dc0dd2cdfa
M1 <m1@192.168.2.33>
2024-01-11 08:54:23 +0700

    update portfolio

---
+++ app/blog-section/blogDetails.ts
@@ -0,0 +1,18 @@
+export type blogProps = {
+  title: string;
+  image: string;
+  date: string;
+  url: string;
+  available: boolean;
+};
+
+export const blogDetails = [
+  // {
+  //   title: "",
+  //   image:
+  //     "",
+  //   date: "DEC 2022",
+  //   url: "",
+  //   available: true,
+  // },
+];
